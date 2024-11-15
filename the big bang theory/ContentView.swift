bd37b86b7a007108b612e680d6da198d8519f13f
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2024-11-15 18:32:47 +0100

    Initial Commit

---
+++ the big bang theory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  the big bang theory
+//
+//  Created by luis david diaz ramirez on 15/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ the big bang theory/the_big_bang_theoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_big_bang_theoryApp.swift
+//  the big bang theory
+//
+//  Created by luis david diaz ramirez on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct the_big_bang_theoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
